246c7c3bfa7377dbb9c6f1afda2775fa7a73ac45
Rajitha Chamod <65697869+TRC98@users.noreply.github.com>
2022-04-02 14:43:35 +0530

    3rd video codding
    Create service folder
    Change Controller

---
+++ TaskAPI/Controllers/TodosController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskAPI.Models;
+using TaskAPI.Services;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private TodoService _todoService;
+        public TodosController()
+        {
+            _todoService = new TodoService();
+        }
+        [HttpGet("{id?}")]
+        public IActionResult GetTodos(int? id)
+        {
+            var mytodos = _todoService.AllTodos();
+
+            if (id == null) return Ok(mytodos);
+
+            mytodos = mytodos.Where(t => t.Id == id).ToList();
+            return Ok(mytodos);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTask()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteTask()
+        {
+            var somethingWrong = true;
+            if (somethingWrong)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ TaskAPI/Services/TodoService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskAPI.Models;
+
+namespace TaskAPI.Services
+{
+    public class TodoService
+    {
+        //Get Todos
+        public List<Todo> AllTodos()
+        {
+            var todos = new List<Todo>();
+            var todo1 = new Todo
+            {
+                Id = 1,
+                Title = "Get books for school",
+                Description = "Get some text books for school",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.Now
+            };
+            todos.Add(todo1);
+            var todo2 = new Todo
+            {
+                Id = 2,
+                Title = "Get Vegitables",
+                Description = "Get vegitable for week",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(2),
+                Status = TodoStatus.Now
+            };
+            todos.Add(todo2);
+            var todo3 = new Todo
+            {
+                Id = 3,
+                Title = "Water the plants",
+                Description = "Water All the plants quikly",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(1),
+                Status = TodoStatus.Now
+            };
+            todos.Add(todo3);
+
+            return todos;
+        }
+    }
+}
